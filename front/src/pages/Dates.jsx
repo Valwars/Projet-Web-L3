874558a7283d00a6a0889bd853de92ccf5090922
Valentin <valentin.merault@gmail.com>
2023-03-28 16:09:47 +0200

    push pull legs

--- front/src/pages/Dates.jsx
+++ front/src/pages/Dates.jsx
@@ -4,53 +4,49 @@ import axios from "axios";
 import $ from "jquery";
 import { datesRoute } from "../utils/APIRoutes";
 import UserProfile from "./userProfile";
-const Dates = ({user,locate}) => {
-    const navigate = useNavigate();    
-   const [leuser, setLeuser] = useState({});
-   const [date , setDate] = useState({});
-   const [tab , setTab] = useState([]);
-   useEffect(()=> {
-     setLeuser(user)
-        const fetchData = async () => {
-            setLeuser(user)
-           console.log(leuser._id);
-           var unid = leuser._id;
-            try {
-                const response = await axios.get(datesRoute, {
-                    params: {
-                        lid : unid
-                    }
-                });
+const Dates = ({ user, locate }) => {
+  const navigate = useNavigate();
+  const [leuser, setLeuser] = useState({});
+  const [date, setDate] = useState({});
+  const [tab, setTab] = useState([]);
+  useEffect(() => {
+    setLeuser(user);
+    const fetchData = async () => {
+      setLeuser(user);
+      console.log(leuser._id);
+      var unid = leuser._id;
+      try {
+        const response = await axios.get(datesRoute, {
+          params: {
+            lid: unid,
+          },
+        });
 
-                console.log(response.data.uti);
-                var premier = response.data.uti.premier;
-                var second = response.data.uti.second;
-                const recup = await axios.get(UserProfile, 
-                    {
-                        params: {
-                            myString : premier
-                        }
-                    })
-                    setTab(tab.concat(recup.data.uti))
-                    console.log(tab)
-            } catch (error) {
-                console.error(error);
-            }
-        }
-        fetchData();
-    },[leuser])
-    
-    return (
-        
-        <div className="app-page">      
-         
-        <div className="dates">
+        console.log(response.data.uti);
+        var premier = response.data.uti.premier;
+        var second = response.data.uti.second;
+
+        const recup = await axios.get(UserProfile, {
+          params: {
+            myString: premier,
+          },
+        });
+        setTab(tab.concat(recup.data.uti));
+        console.log(tab);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, [leuser]);
+
+  return (
+    <div className="app-page">
+      <div className="dates">
         <h2>La liste de vos dates</h2>
-        
-     </div>      
-          </div>
-   
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Dates;
