a4d2587311e10fac89d81a7d40d0d3ee583668c0
Valentin <valentin.merault@gmail.com>
2023-05-13 19:41:21 +0200

    dates et update visu

--- front/src/pages/Dates.jsx
+++ front/src/pages/Dates.jsx
@@ -1,15 +1,15 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import $ from "jquery";
+import Loader_transition from "../components/Loading";
 import { datesRoute } from "../utils/APIRoutes";
 import UserProfile from "./userProfile";
 const Dates = ({ user, locate }) => {
-
   const navigate = useNavigate();
   const [leuser, setLeuser] = useState({});
   const [date, setDate] = useState([]);
   const [couple, setTab] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     setLeuser(user);
@@ -24,73 +24,78 @@ const Dates = ({ user, locate }) => {
           },
         });
 
-        console.log("response",response.data.date);
+        console.log("response", response.data.date);
         var tab = response.data.date;
         var coupletab = response.data.couple;
         setDate(tab);
-        console.log("tab date",date);
+        console.log("tab date", date);
         setTab(coupletab);
-        console.log("couple",couple);
-
+        console.log("couple", couple);
+        setLoading(false);
       } catch (error) {
         console.error(error);
       }
-    }
+    };
     fetchData();
-  }, [leuser])
-
-
+  }, [leuser]);
 
   return (
-
     <div className="app-page">
-      <div className="match-content">
-        <div className="top">
-          <h1>Vos dates</h1>
-          <input type="text" placeholder="Rechercher..." />
-        </div>
-        <div className="matchs-container">
-          {
-            date.map((date) => {
+      {loading ? (
+        <Loader_transition></Loader_transition>
+      ) : (
+        <div className="match-content">
+          <div className="top">
+            <h1>Vos dates</h1>
+            <div className="order-filters">
+              <i className="fa fa-chevron-down"></i>
+
+              <i className="fa fa-chevron-up"></i>
+            </div>
+            <input id="convtp" type="text" placeholder="Rechercher..." />
+          </div>
+          <div className="matchs-container">
+            {date.map((date) => {
               // if (user.personnes[0] != undefined) {
-                return (
-                  <div
-                    className="match"
-                    onClick={() => {
-                      // setLocate("/match");
-                      navigate("/user-profile/2");
-                    }}
-                  >
-                    {couple.map((u) => {
-
-                      if ( u.premier._id == date.premier._id && u.second._id == date.second._id){
-                         return (
-                        <>
-                        <img src={u.premier.pdp} alt="" />
-                          {/* <img src={u.premier.pdp} alt="" />
-                          <h2>{u.premier.name + " " + u.premier.firstname}</h2>
-                          <img src={u.second.pdp} alt="" />
-                          <h2>{u.second.name + " " + u.second.firstname}</h2> */}
-                        </>
-                      )
-                       } else {
-                        return null;
-                      }
-                     
-                    })}
-                    <p>{date.localisation}</p>
-
-                    <p>{date.date}</p>
+              return (
+                <div
+                  className="match date_cnt"
+                  onClick={() => {
+                    // setLocate("/match");
+                    navigate("/user-profile/2");
+                  }}
+                >
+                  <div className="user-date">
+                    <div>
+                      <img
+                        src="https://randomuser.me/api/portraits/women/22.jpg"
+                        alt=""
+                      />
+                      <h2>Jane MaBite</h2>
+                    </div>
                   </div>
-                )
+
+                  <div className="dateInfo">
+                    <div>
+                      <i class="fas fa-clock"></i>
+
+                      <p>{date.localisation}</p>
+                    </div>
+                    <div>
+                      <i class="fas fa-map-marker-alt"></i>
+
+                      <p>{date.date}</p>
+                    </div>
+                  </div>
+                </div>
+              );
               // } else {
               //   return null;
               // }
-            })
-          }
-
+            })}
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
